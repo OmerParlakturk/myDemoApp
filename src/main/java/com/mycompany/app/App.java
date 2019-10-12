797679e2b4365ee2c2df1502edf30943bfb0ac5c
OmerParlakturk <head2story@gmail.com>
2019-10-12 18:13:59 +0300

    I have started my awesome projectgit statusgit status It will be great

---
+++ src/main/java/com/mycompany/app/App.java
@@ -0,0 +1,29 @@
+package com.mycompany.app;
+import java.util.*;
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+   public static boolean myMethod(ArrayList<Integer> a1, ArrayList<Integer> a2, int i1, int i2)
+// this method check the sum of first i1 elements in array a1 and compare to sum of i2 elements in array a2, if the sum will be equal then it returns true;
+   {
+	int tempSum1 = 0;
+	int tempSum2 = 0;
+
+	for(int i = 0 ; i< i1 ; i++){
+	        tempSum1 = a1.get(i);
+	}
+	for(int i = 0 ; i< i2 ; i++){
+		tempSum2 = a2.get(i);
+	}
+
+	return (tempSum1 == tempSum2);
+
+   }
+   public static void main( String[] args )
+   {
+	System.out.println( "Hello World" );
+   }
+}
---
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -0,0 +1,48 @@
+package com.mycompany.app;
+import java.util.*;
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+    extends TestCase
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public AppTest( String testName )
+    {
+        super( testName );
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite( AppTest.class );
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testApp()
+    {
+        assertTrue( true );
+    }
+    /*
+    public void testResult(){
+    	ArrayList<Integer> array1 = new ArrayList<>(Arrays.asList(1,2,3,4,5,6,7,8));
+	ArrayList<Integer> array2 = new ArrayList<>(Arrays.asList(3,3,4,5,6,7,8,9,10));
+
+	assertTrue(new App().myMethod(array1,array2, 5,4) );
+    }*/
+
+
+
+}
